f2f80b44fc88509ed858b72b409878e43f360b32
413qq <2522707259@qq.com>
2026-09-16 21:52:35 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const PREFIX='chaoxi-pages-'+new URL(self.registration.scope).pathname+'|';
-const CACHE=PREFIX+'v2';
+const CACHE=PREFIX+'v3';
 const files=['./','./index.html','./app.js','./app.css','./portraits.png','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./manifest.webmanifest','./favicon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(files))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k))))));
