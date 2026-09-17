cbf2b012c27aa9c00b6f8100326eb445843f3fd6
413qq <2522707259@qq.com>
2026-09-17 20:32:47 +0800

    更新朝夕 1.3.2：优化移动端页面与人物设定

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 const PREFIX='chaoxi-pages-'+new URL(self.registration.scope).pathname+'|';
-const CACHE=PREFIX+'v3';
+const CACHE=PREFIX+'v6';
 const files=['./','./index.html','./app.js','./app.css','./portraits.png','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./manifest.webmanifest','./favicon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(files))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(PREFIX)&&k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;event.respondWith(fetch(event.request).then(r=>{if(r.ok){const copy=r.clone();event.waitUntil(caches.open(CACHE).then(c=>c.put(event.request,copy)))}return r}).catch(()=>caches.match(event.request).then(r=>r||(event.request.mode==='navigate'?caches.match('./index.html'):Response.error()))))});
+
+
